1e9bb7eec6b7fada17e925929a341ed287380e75
Deepak Mehar <deepakmehar397@gmail.com>
2024-08-03 01:56:12 +0530

    test

--- src/components/common/ListCard.js
+++ src/components/common/ListCard.js
@@ -10,7 +10,7 @@ const ListCard = () => {
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const response = await axios.get("../jobs.json"); // Adjust path as necessary
+        const response = await axios.get("/jobs.json"); // Adjust path as necessary
         setJobs(response.data);
         setFilteredJobs(response.data); // Initialize filteredJobs with all jobs
       } catch (error) {
