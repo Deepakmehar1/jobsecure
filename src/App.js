05b19216ad32529e1932f323dc58d88f816a1155
Deepakmehar1 <155355171+Deepakmehar1@users.noreply.github.com>
2024-08-03 20:02:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IndexPage from "./components/IndexPage";
 import LoginPage from "./components/LoginPage";
-import ListCard from "./components/common/ListCard";
 import JobsCard from "./components/Jobs";
 import SideBar from "./components/common/SideBar";
 import CandidateHomePage from "./components/CandidateHomePage";
@@ -16,7 +15,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/lists" element={<ListCard />} />
         <Route path="/jobs" element={<JobsCard />} />
         <Route path="/candidate/home" element={<CandidateHomePage />} />
       </Routes>
